d688a94084f7d52f2a791b288e7aa4a5408cbf1d
pwnChook <56835624+Kettn@users.noreply.github.com>
2023-02-28 16:13:35 +0800

    Update Share-finder.ps1
    Invoke-Sharefinder > Invoke-ContributionSpotter

--- Share-finder.ps1
+++ Share-finder.ps1
@@ -66,93 +66,6 @@ function func
 
 function Add-Win32Type
 {
-    <#
-        .SYNOPSIS
-
-        Creates a .NET type for an unmanaged Win32 function.
-
-        Author: Matthew Graeber (@mattifestation)
-        License: BSD 3-Clause
-        Required Dependencies: None
-        Optional Dependencies: func
- 
-        .DESCRIPTION
-
-        Add-Win32Type enables you to easily interact with unmanaged (i.e.
-        Win32 unmanaged) functions in PowerShell. After providing
-        Add-Win32Type with a function signature, a .NET type is created
-        using reflection (i.e. csc.exe is never called like with Add-Type).
-
-        The 'func' helper function can be used to reduce typing when defining
-        multiple function definitions.
-
-        .PARAMETER DllName
-
-        The name of the DLL.
-
-        .PARAMETER FunctionName
-
-        The name of the target function.
-
-        .PARAMETER ReturnType
-
-        The return type of the function.
-
-        .PARAMETER ParameterTypes
-
-        The function parameters.
-
-        .PARAMETER NativeCallingConvention
-
-        Specifies the native calling convention of the function. Defaults to
-        stdcall.
-
-        .PARAMETER Charset
-
-        If you need to explicitly call an 'A' or 'W' Win32 function, you can
-        specify the character set.
-
-        .PARAMETER SetLastError
-
-        Indicates whether the callee calls the SetLastError Win32 API
-        function before returning from the attributed method.
-
-        .PARAMETER Module
-
-        The in-memory module that will host the functions. Use
-        New-InMemoryModule to define an in-memory module.
-
-        .PARAMETER Namespace
-
-        An optional namespace to prepend to the type. Add-Win32Type defaults
-        to a namespace consisting only of the name of the DLL.
-
-        .EXAMPLE
-
-        $Mod = New-InMemoryModule -ModuleName Win32
-
-        $FunctionDefinitions = @(
-        (func kernel32 GetProcAddress ([IntPtr]) @([IntPtr], [String]) -Charset Ansi -SetLastError),
-        (func kernel32 GetModuleHandle ([Intptr]) @([String]) -SetLastError),
-        (func ntdll RtlGetCurrentPeb ([IntPtr]) @())
-        )
-
-        $Types = $FunctionDefinitions | Add-Win32Type -Module $Mod -Namespace 'Win32'
-        $Kernel32 = $Types['kernel32']
-        $Ntdll = $Types['ntdll']
-        $Ntdll::RtlGetCurrentPeb()
-        $ntdllbase = $Kernel32::GetModuleHandle('ntdll')
-        $Kernel32::GetProcAddress($ntdllbase, 'RtlGetCurrentPeb')
-
-        .NOTES
-
-        Inspired by Lee Holmes' Invoke-WindowsApi http://poshcode.org/2189
-
-        When defining multiple function prototypes, it is ideal to provide
-        Add-Win32Type with an array of function signatures. That way, they
-        are all incorporated into the same in-memory module.
-    #>
-
     [OutputType([Hashtable])]
     Param(
         [Parameter(Mandatory = $True, ValueFromPipelineByPropertyName = $True)]
@@ -199,7 +112,6 @@ function Add-Win32Type
 
     PROCESS
     {
-        # Define one type for each DLL
         if (!$TypeHash.ContainsKey($DllName))
         {
             if ($Namespace)
@@ -212,13 +124,11 @@ function Add-Win32Type
             }
         }
 
-        $Method = $TypeHash[$DllName].DefineMethod(
             $FunctionName,
             'Public,Static,PinvokeImpl',
             $ReturnType,
             $ParameterTypes)
 
-        # Make each ByRef parameter an Out parameter
         $i = 1
         foreach($Parameter in $ParameterTypes)
         {
@@ -236,7 +146,6 @@ function Add-Win32Type
         $CharsetField = $DllImport.GetField('CharSet')
         if ($SetLastError) { $SLEValue = $True } else { $SLEValue = $False }
 
-        # Equivalent to C# version of [DllImport(DllName)]
         $Constructor = [Runtime.InteropServices.DllImportAttribute].GetConstructor([String])
         $DllImportAttribute = New-Object Reflection.Emit.CustomAttributeBuilder($Constructor,
             $DllName, [Reflection.PropertyInfo[]] @(), [Object[]] @(),
@@ -624,7 +533,7 @@ function Get-NetShare {
 }
 
 
-function Invoke-ShareFinder {
+function Invoke-ContributionSpotter {
     
     [CmdletBinding()]
     param(
@@ -690,7 +599,7 @@ function Invoke-ShareFinder {
         $targetDomain = Get-NetDomain
     }
     
-    Write-Verbose "[*] Running ShareFinder on domain $targetDomain with delay of $Delay"
+    Write-Verbose "[*] Running ContributionSpotter on domain $targetDomain with delay of $Delay"
     $servers = @()
 
     if($HostList){
@@ -775,7 +684,7 @@ function Invoke-ShareFinder {
 }
 
 
-function Invoke-ShareFinderThreaded {
+function Invoke-ContributionSpotterThreaded {
   
     [CmdletBinding()]
     param(
